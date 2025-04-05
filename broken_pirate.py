2f0410580983aa73105694831aef90457adaef3e
kiya <kiyaa@kiyas-Laptop.local>
2025-04-05 21:37:44 +0400

    Fix broken_pirate.py logic

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,7 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+greeting = input("Hello, possible pirate! What's the password? ")
+
+if greeting == "Arrr!":
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
+
